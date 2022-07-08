5b81298e5a278c430baee7fb0111583c9acfbed5
Husnan Alam <alam@memoriki.com>
2022-07-08 10:28:52 +0800

    1.8 exercise completed

--- exercise1/src/App.js
+++ exercise1/src/App.js
@@ -1,4 +1,27 @@
-import { useState } from "react"
+import { useState } from "react";
+
+const Statistics = (props) => {
+  if (props.all === 0) {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>Good {props.good}</p>
+      <p>Neutral {props.neutral}</p>
+      <p>Bad {props.bad}</p>
+      <p>All {props.all}</p>
+      <p>Average {props.average}</p>
+      <p>Positive {props.positive} %</p>
+    </div>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -47,13 +70,7 @@ const App = () => {
       <button onClick={goodClick}>Good</button>
       <button onClick={neutralClick}>Neutral</button>
       <button onClick={badClick}>Bad</button>
-      <h2>Statistics</h2>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>All {all}</p>
-      <p>Average {average}</p>
-      <p>Positive {positive} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} />
     </div>
   )
 }
