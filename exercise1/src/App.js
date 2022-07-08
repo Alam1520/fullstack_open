8cc736ea00c2f5a4c322b9bf6c19620c5d1809b8
Husnan Alam <alam@memoriki.com>
2022-07-08 10:26:07 +0800

    excersice 1.7 completed

--- exercise1/src/App.js
+++ exercise1/src/App.js
@@ -1,59 +1,59 @@
-const Header = (props) => {
-  return (
-    <h1>
-      {props.course.name}
-    </h1>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      {props.course.parts.map(part => <Part key={part.name} part={part.name} exercises={part.exercises} />)}
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <p>
-      Number of exercises {props.course.parts.reduce((acc, curr) => acc + curr.exercises, 0)}
-    </p>
-  )
-}
+import { useState } from "react"
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
+
+  const goodClick = () => {
+    setGood(good + 1);
+    setAll(all + 1);
+    setPositive(positive / all);
+    averageClick();
+    positivePercentage();
+  }
+
+  const neutralClick = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+    setPositive(positive / all);
+    averageClick();
+    positivePercentage();
+  }
+
+  const badClick = () => {
+    setBad(bad + 1);
+    setAll(all + 1);
+    setPositive(positive / all);
+    averageClick();
+    positivePercentage();
+  }
+
+  const averageClick = () => {
+    setAverage((good - bad) / all);
+  }
+
+  const positivePercentage = () => {
+    setPositive(good / all);
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <h1>Give FeedBack</h1>
+      <button onClick={goodClick}>Good</button>
+      <button onClick={neutralClick}>Neutral</button>
+      <button onClick={badClick}>Bad</button>
+      <h2>Statistics</h2>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>All {all}</p>
+      <p>Average {average}</p>
+      <p>Positive {positive} %</p>
     </div>
   )
 }
