749b3334b2adcfab2223a7ec6f4f575a24ba0756
Husnan Alam <alam@memoriki.com>
2022-07-08 11:00:22 +0800

    completed 1.14

--- exercise1_12/src/App.js
+++ exercise1_12/src/App.js
@@ -29,12 +29,16 @@ const App = () => {
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
       <br />
       <p>has {votes[selected]} votes</p>
       <br />
       <button onClick={vote} >vote</button>
       <button onClick={nextAnecdote}>Next anecdote</button>
+      <br />
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[votes.indexOf(Math.max(...votes))]}
     </div>
   )
 }
