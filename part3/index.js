62f9eb8fd7b79bfab9123b9e48769c86f599ae81
Husnan Alam <alam@memoriki.com>
2022-07-29 15:10:18 +0800

    small change to version number

--- part3/index.js
+++ part3/index.js
@@ -1,27 +1,10 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const Note = require('./models/note');
+const { response } = require('express');
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        date: "2022-05-30T17:30:31.098Z",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2022-05-30T18:39:34.091Z",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2022-05-30T19:20:14.298Z",
-        important: true
-    }
-];
+const app = express();
 
 app.use(express.json());
 app.use(cors());
@@ -37,18 +20,23 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
+    Note.find({}).then(notes => {
+        res.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const note = notes.find(note => note.id === id);
-
-    if (note) {
-        res.json(note)
-    } else {
-        res.status(404).end();
-    }
+    Note.findById(req.params.id).then(note => {
+        if (note) {
+            res.json(note)
+        } else {
+            res.status(404).end()
+        }
+    })
+        .catch(error => {
+            console.log(error)
+            response.status(500).end()
+        })
 })
 
 app.delete('/api/notes/:id', (req, res) => {
@@ -60,22 +48,21 @@ app.delete('/api/notes/:id', (req, res) => {
 
 app.post('/api/notes', (req, res) => {
     const body = req.body;
-    if (!body.content) {
+    if (body.content === undefined) {
         return res.status(400).json({
             error: 'content missing'
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId(),
-    }
+    })
 
-    notes = notes.concat(note);
-
-    res.json(note)
+    note.save().then(savedNote => {
+        res.json(savedNote);
+    })
 })
 
 const unknownEndpoint = (req, res) => {
@@ -84,7 +71,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
